bdd0115cd4c234e187b8f70a70fd2ebcc8cdd8d7
Magarevedant <92874562+Magarevedant@users.noreply.github.com>
2026-07-25 07:43:33 +0000

    ping endpoint v2

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,5 @@
 // Ping handler at url/api/ping
 
-export function handler(req,res){
-    res.send('SaaS Pinged!!!')
+export default function handler(req, res) {
+  res.status(200).json({ message: 'SaaS Pinged!!!' })
 }
